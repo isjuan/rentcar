6ea712d4b701b6431d3790307602bf21490af9f2
Juan Olguins <olguinsjuan@gmail.com>
2021-05-02 13:13:23 -0300

    Criando alguns botoes de OK
    Botões de OK em telas que já existem...

--- limite/tela_cliente.py
+++ limite/tela_cliente.py
@@ -54,24 +54,34 @@ class TelaCliente():
 
   def exclui_cliente_return(self, verificador):
     if verificador == 0:
-     layout= [[sg.Text('Nome invalido! Retornando a tela Cliente')]]
+     layout= [[sg.Text('Nome invalido! Retornando a tela Cliente')],
+              [sg.Button('OK', key=self.close(), size=(5, 1))]
+              ]
     if verificador == 1:
-     layout= [[sg.Text('O cliente tem um aluguel ativo! Falha ao remover.')]]
+     layout= [[sg.Text('O cliente tem um aluguel ativo! Falha ao remover.')],
+              [sg.Button('OK', key=self.close(), size=(5, 1))]
+              ]
     if verificador == 2:
-     layout= [[sg.Text('Cliente removido do cadastro!')]]
+     layout= [[sg.Text('Cliente removido do cadastro!')],
+              [sg.Button('OK', key=self.close(), size=(5, 1))]
+              ]
 
     self.__window = sg.Window('Exclui cliente').Layout(layout)
     botao, valores = self.__window.Read()
 
   def aluguel_erro(self):
-    layout= [[sg.Text('O nome do cliente informado nao foi encontrado!')]]
+    layout= [[sg.Text('O nome do cliente informado nao foi encontrado!')],
+             [sg.Button('OK', key=self.close(), size=(5, 1))]
+             ]
     self.__window = sg.Window('Erro ao criar aluguel').Layout(layout)
     botao, valores = self.__window.Read()
 
   def lista_alugueis(self, lis: list):
     total = len(lis)
     if total == 0:
-      layout= [[sg.Text('Nenhum aluguel com o nome de cliente informado!')]]
+      layout= [[sg.Text('Nenhum aluguel com o nome de cliente informado!')],
+               [sg.Button('OK', key=self.close(), size=(5, 1))]
+               ]
     else:
       contador = 0
       layout= [[sg.Text("Os alugueis desse cliente são:")]]
@@ -92,15 +102,19 @@ class TelaCliente():
         layout.append([sg.Text("Cliente"), sg.Text(contador)])
         layout.append([sg.Text("Nome:"), sg.Text(i[0]), sg.Text("Telefone:"),sg.Text(i[1]), sg.Text("Endereco:"),sg.Text(i[2])])   
         contador = contador + 1 
+layout.append([sg.Button('<< Retornar <<', key= self.close(), size=(15, 1))])
 
     self.__window = sg.Window('Listar clientes').Layout(layout)
     botao, valores = self.__window.Read()
 
   def incluir_cliente_return(self, verificador):
     if verificador == True:   
-      layout= [[sg.Text('Ja existe um cliente com esse nome!')]]
+      layout= [[sg.Text('Ja existe um cliente com esse nome!')],
+               [sg.Button('OK', key=self.close(), size=(5, 1))]]
     if verificador == False:
-      layout= [[sg.Text('Cliente cadastrado!')]]
+      layout= [[sg.Text('Cliente cadastrado!')],
+               [sg.Button('OK', key=self.close(), size=(5, 1))]
+               ]
 
     self.__window = sg.Window('Incluir cliente').Layout(layout)
     botao, valores = self.__window.Read()
